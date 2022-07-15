c6aee39faed330bfff2eab38cc9a8a434740d302
edibrum <edineiabrum.edi@gmail.com>
2022-07-15 17:30:55 -0300

    teste do clone

--- desligador.c
+++ desligador.c
@@ -1,26 +1,27 @@
 #include <stdio.h>
 
-  
+
 int main (){
+	//Cria as variáveis que utilizaremos:
 	int opt;
-printf("******Desligador de Computador ******\n*************************************\n*************************************\n******** Digite a opcao desejada*****\n*************************************\n***1 = Desligar o Computador*********\n*************************************\n***2 = Reiniciar o Computador********\n*************************************\n***3 = Anular  o Desligamento********\n*************************************\n***4 = Fechar o Programa ************\n*************************************\n	");
-	//caso voce seja snob e use linux, use as linhas comentadas
+	//Mostra e pede ao usuário que escolha uma opção, e recebe a opção na variárel opt:
+    printf("******Desligador de Computador ******\n*************************************\n*************************************\n******** Digite a opcao desejada*****\n*************************************\n***1 = Desligar o Computador*********\n*************************************\n***2 = Reiniciar o Computador********\n*************************************\n***3 = Anular  o Desligamento********\n*************************************\n***4 = Fechar o Programa ************\n*************************************\n	");
 	scanf("%d", &opt);
-	
+
+
+	//Mostra ao usuário a opção selecionada e executa o comando correspondente:
 	printf("A opcao selecionada foi %d! ", opt);
 	switch (opt){
 		case 1:
 			system("shutdown -s -t 1200");
-			//system("shutdown -h +1200");
 			break;
-		case 2: 
+		case 2:
 			system("shutdown -r -t 1200");
-			//system("reboot -h +1200");
-		case 3: 
+			break;
+        case 3:
 			system("shutdown -a");
-			//system("reboot -c");
-			//system("shutdown -c");
-		default: 
+			break;
+		default:
 			system("exit");
 	}
 		return 0;
