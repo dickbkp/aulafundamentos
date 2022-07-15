71a730ab6685188b686091df45b1fd212c1d0f19
v4van1lla <vitorrbmh@gmail.com>
2022-07-15 17:23:44 -0300

    cincoquarenta

--- desligador.c
+++ desligador.c
@@ -1,26 +1,29 @@
 #include <stdio.h>
 
-  
+
 int main (){
 	int opt;
-printf("******Desligador de Computador ******\n*************************************\n*************************************\n******** Digite a opcao desejada*****\n*************************************\n***1 = Desligar o Computador*********\n*************************************\n***2 = Reiniciar o Computador********\n*************************************\n***3 = Anular  o Desligamento********\n*************************************\n***4 = Fechar o Programa ************\n*************************************\n	");
-	//caso voce seja snob e use linux, use as linhas comentadas
+
+    printf("Desligador de Computador \n\n\nDigite a opcao desejada \n\n1 = Desligar o Computador \n\n2 = Reiniciar o Computador \n\n3 = Anular  o Desligamento \n\n4 = Fechar o Programa \n\n	");
+
 	scanf("%d", &opt);
-	
+
 	printf("A opcao selecionada foi %d! ", opt);
-	switch (opt){
-		case 1:
+        switch (opt){
+            case 1:
 			system("shutdown -s -t 1200");
 			//system("shutdown -h +1200");
-			break;
-		case 2: 
+                break;
+            case 2:
 			system("shutdown -r -t 1200");
 			//system("reboot -h +1200");
-		case 3: 
+                break;
+            case 3:
 			system("shutdown -a");
 			//system("reboot -c");
 			//system("shutdown -c");
-		default: 
+                break;
+                    default:
 			system("exit");
 	}
 		return 0;
