8d79fde13c13030732c782d8b9f16c0e6a489253
Luiz <luizh.duarte@hotmail.com>
2022-07-15 17:28:20 -0300

    v3

--- desligador.c
+++ desligador.c
@@ -4,7 +4,6 @@
 int main (){
 	int opt;
 printf("******Desligador de Computador ******\n*************************************\n*************************************\n******** Digite a opcao desejada*****\n*************************************\n***1 = Desligar o Computador*********\n*************************************\n***2 = Reiniciar o Computador********\n*************************************\n***3 = Anular  o Desligamento********\n*************************************\n***4 = Fechar o Programa ************\n*************************************\n	");
-	//caso voce seja snob e use linux, use as linhas comentadas
 	do
         {
 	scanf("%d", &opt);
@@ -13,17 +12,13 @@ printf("******Desligador de Computador ******\n*********************************
 	switch (opt){
 		case 1:
 			system("shutdown -s -t 1200");
-			//system("shutdown -h +1200");
 			break;
 		case 2:
 			system("shutdown -r -t 1200");
-			//system("reboot -h +1200");
 			break;
 		case 3:
 			system("shutdown -a");
 			break;
-			//system("reboot -c");
-			//system("shutdown -c");
 		default:
 		    break;
 	}
